1325de3137733b625ce5411ddaa28355a09e9929
MilkySin <blannkisblank@gmail.com>
2022-10-06 18:52:42 +0700

    add githubrepos

---
+++ github.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+from urllib import request
+import requests
+import json
+
+
+def get_name():
+    username = input("Username: ")
+    return username
+
+
+def get_repo(username):
+    repos: list[str] = []
+    s = requests.get(f"https://api.github.com/users/{username}/repos")
+    try:
+        repos = [
+            dic["full_name"][dic["full_name"].rfind("/") + 1 :]
+            for dic in json.loads(s.text)
+        ]
+    except TypeError:
+        print(f"User {username} doesn't exist")
+    return repos
+
+
+def main():
+    username = get_name()
+    repos = get_repo(username)
+    for i in repos:
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
