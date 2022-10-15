3d843d0c069d4e932f0df3f5b3b5ea2aeb4b1bea
MilkySin <blannkisblank@gmail.com>
2022-10-15 18:36:49 +0700

    fixed

--- beermap.py
+++ beermap.py
@@ -1,63 +1,63 @@
 #!/usr/bin/env python3
 
 import json
+from textwrap import indent
 import requests
 import os
 import time
 
 
-def config(key_word="bia", language="vi"):
-    lat, long = 21.012832699039034, 105.82177484220092
+def config():
+    key_word = "bia"
+    language = "vi"
+    lat = 21.012832699039034
+    long = 105.82177484220092
     radius = 2000
-    ses = requests.Session()
-    timeout_time = 15
-    return key_word, lat, long, radius, ses, timeout_time, language
+    return key_word, language, lat, long, radius
 
 
 def api_key():
     with open(os.path.join(os.path.dirname(__file__), "api_key.txt")) as f:
         key = f.read().strip()
 
-    print(key)
     return key
 
 
-def first_api_call(key_word, lat, long, radius, ses, timeout_time, language):
-    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?keyword={key_word}&location={lat}, {long}&radius={radius}&key={key_word}&language={language}"
+def first_api_call(key_word, lat, long, radius, language, key):
+    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={lat}, {long}&radius={radius}&keyword={key_word}&key={key}&language={language}"
     payload = {}
     header = {}
-    resp = ses.request("GET", url, headers=header, data=payload, timeout=timeout_time)
+    resp = requests.request("GET", url, headers=header, data=payload, timeout=20)
     resp = json.loads(resp.text)
     location_list = resp["results"]
-
+    token = resp["next_page_token"]
     if "next_page_token" not in resp or len(resp["next_page_token"]) == 0:
-        next_page_token = "No more result"
+        token = "No more result"
     else:
-        next_page_token = resp["next_page_token"]
+        token = resp["next_page_token"]
 
-    return location_list, next_page_token
+    return location_list, token
 
 
-def next_api_call(ses, timeout_time, next_token, key_word):
-    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken={next_token}&key={key_word}"
+def next_api_call(token, key):
+    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken={token}&key={key}"
     payload = {}
     header = {}
-    resp = ses.request("GET", url, headers=header, data=payload, timeout=timeout_time)
+    resp = requests.request("GET", url, headers=header, data=payload, timeout=20)
     resp = json.loads(resp.text)
     location_list = resp["results"]
-
     if "next_page_token" not in resp or len(resp["next_page_token"]) == 0:
-        next_page_token = "No more result"
+        token = "No more result"
     else:
-        next_page_token = resp["next_page_token"]
+        token = resp["next_page_token"]
 
-    return location_list, next_page_token
+    return location_list, token
 
 
 def get_address(location):
     lat = location["geometry"]["location"]["lat"]
     long = location["geometry"]["location"]["lng"]
-    coord = [lat, long]
+    coord = [long, lat]
     name = location["name"]
     st_ward = location["vicinity"]
 
@@ -73,11 +73,11 @@ def get_address(location):
     return name, coord, address
 
 
-def formatfeature(coordinates, name, address):
+def formatfeature(name, coord, address):
     feature = {
         "type": "Feature",
-        "geometry": {"type": "Point", "coordinates": coordinates},
-        "properties": {"address": address, "name": name},
+        "geometry": {"type": "Point", "coordinates": coord},
+        "properties": {"address": address, "name": name}
     }
     return feature
 
@@ -88,41 +88,30 @@ def collectfeature(feature, features):
 
 
 def writefile(data):
+    file = json.dumps(data, indent=2)
     with open("pymi_beer.geojson", "w", encoding="utf-8") as f:
-        f.write(str(data))
+        f.write(file)
     return
 
 
 def main():
-    ses, timeout_time, lat, long, radius, keyword, language = config()
+    key_word, language, lat, long, radius = config()
     key = api_key()
-    location_list, next_page_token = first_api_call(
-        ses,
-        timeout_time,
-        lat,
-        long,
-        radius,
-        keyword,
-        language,
-    )
-
+    location_list, token = first_api_call(key_word, lat, long, radius, language, key)
     number_of_locations = 50
-
     features = {"type": "FeatureCollection", "features": []}
 
     while number_of_locations > len(location_list):
-        if next_page_token == "No more result":
+        if token == "No more result":
             break
         else:
-            time.sleep(2)
-            next_location_list, next_page_token = next_api_call(
-                ses, timeout_time, next_page_token, key
-            )
+            time.sleep(5)
+            next_location_list, token = next_api_call(token, key)
             location_list.extend(next_location_list)
 
     for location in location_list[:50]:
-        coordinates, name, address = get_address(location)
-        feature = formatfeature(coordinates, name, address)
+        name, coord, address = get_address(location)
+        feature = formatfeature(name, coord, address)
         collectfeature(feature, features)
 
     writefile(features)
